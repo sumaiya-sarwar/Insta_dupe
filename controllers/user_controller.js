03feec17a94bd3f82f86d29c0c0580d8ea9d1f59
Manases <manasesgil95@gmail.com>
2022-08-29 10:09:51 -0500

    login page and some changes on the server

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { appendFile } = require('fs');
+const { model } = require('mongoose');
 const router = express.Router();
 
 router.use(express.json());
 
 router.use(express.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    res.render('home.ejs')
+router.get('/login',(req, res) => {
+    res.render('login.ejs');
 })
 
-module.exports = router; 
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express'); // commonJS import statement
 const methodOverride = require('method-override');
 require('./config/db.connection');
+const userControllers = require('./controllers/user_controller');
+const mainController = require('./controllers/main_controller');
 
 const app = express();
 const PORT = 4000;
@@ -9,10 +11,8 @@ app.set('view engine', 'ejs');
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
-
-app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
+app.use('', userControllers);
+app.use('/', mainController);
 
 app.get('/*', (req, res) => {
     res.render('404.ejs')
