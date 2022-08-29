b95fca292d47a54a4f88ca16190844e4e7354e4e
Manases <manasesgil95@gmail.com>
2022-08-29 09:20:39 -0500

    app express is up and running

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express'); // commonJS import statement
+const methodOverride = require('method-override');
+require('./config/db.connection');
 
+const app = express();
+const PORT = 4000;
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/*', (req, res) => {
+    res.render('404.ejs')
+})
+
+app.listen(PORT, () => console.log('starting server at port:', PORT));
